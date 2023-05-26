c810de82acfb360548ce690f8ca49662cb9b5bd1
kudas ganesan <kudosdesigner@hotmail.com>
2023-05-26 09:33:57 -0400

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, TextInput, Button, Alert } from 'react-native';
+import axios from 'axios';
 
-export default function App() {
+const App = () => {
+  const [inputText, setInputText] = useState('');
+
+  const handleInputChange = (text) => {
+    setInputText(text);
+  };
+
+  const handleSubmit = async () => {
+    try {
+      const response = await axios.post('http://192.168.7.10:8088/API/?Function=OverlayInput4In&input=DoNotEdit', { message: inputText });// http://192.168.7.10:8088/API/?Function=OverlayInput4In&input=DoNotEdit
+      if (response.status === 200) {
+        Alert.alert('Submitted', inputText);
+        setInputText('');
+      } else {
+        Alert.alert('Error', 'Failed to submit');
+      }
+    } catch (error) {
+      Alert.alert('Error', error.message);
+    }
+  };
+
+  const handleDelete = () => {
+    setInputText('');
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <TextInput
+        style={{ width: '80%', height: 40, borderWidth: 1, marginBottom: 10, paddingHorizontal: 10 }}
+        placeholder="Enter text"
+        value={inputText}
+        onChangeText={handleInputChange}
+      />
+      <Button title="Submit" onPress={handleSubmit} />
+      <Button title="Delete" onPress={handleDelete} />
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
